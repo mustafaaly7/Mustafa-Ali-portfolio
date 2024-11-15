8175b90c7418b1b5a7f1f854db3467356bee2f7e
Mustafa Ali <mustafaaaly27@gmail.com>
2024-11-15 14:24:15 +0500

    added fuctionable form

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,12 +3,13 @@ export default function Contact() {
         <section id="contact" className="py-20 bg-black text-light-gray">
             <div className="container mx-auto px-6">
                 <h2 className="text-2xl font-bold text-white mb-6 ">Contact Me</h2>
-                <form className="bg-gray-800 p-6 rounded-lg shadow-lg" action="https://formsubmit.co/el/wuvivi" method="POST">
+                <form className="bg-gray-800 p-6 rounded-lg shadow-lg" action="https://formsubmit.co/mustafaaaly27@gmail.com" method="POST">
                     <div className="mb-4">
                         <label htmlFor="name" className="text-gray-400">Name</label>
                         <input
                             type="text"
                             id="name"
+                            name="name" // Add name attribute
                             placeholder="Enter Your Name"
                             className="w-full bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
@@ -18,6 +19,7 @@ export default function Contact() {
                         <input
                             type="email"
                             id="email"
+                            name="email" // Add name attribute
                             placeholder="Enter Your Email Address"
                             className="w-full bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
@@ -26,6 +28,7 @@ export default function Contact() {
                         <label htmlFor="message" className="text-gray-400">Message</label>
                         <textarea
                             id="message"
+                            name="message" // Add name attribute
                             placeholder="Write Your Message Here"
                             className="w-full bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         ></textarea>
