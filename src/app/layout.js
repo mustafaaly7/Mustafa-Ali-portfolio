e8f4793ea0e81a77418af19bbc59f4ed3b6dfb26
Mustafa Ali <mustafaaaly27@gmail.com>
2025-01-03 04:33:02 +0500

    added discord verification meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="p:domain_verify" content="dh=157f065e65e1349ec323709ddd833d58117edb7c" />
+        <title>Your Portfolio</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
