9d598e5df6e7a76788dc93174610e40023e2a40c
Mustafa Ali <mustafaaaly27@gmail.com>
2025-08-30 11:36:46 +0500

    added google analytics code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { icons } from 'lucide-react'
 import './globals.css'
 import { Inter, Roboto_Mono } from 'next/font/google'
+import Script from 'next/script'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,11 +23,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-WCB6PFJFB1"></Script>
+<Script>
+{`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
 
+  gtag('config', 'G-WCB6PFJFB1');`}
+</Script>
         <meta name="p:domain_verify" content="dh=157f065e65e1349ec323709ddd833d58117edb7c" />
         <meta name="p:domain_verify" content="307cc5d6404d5724d9d08b8f78395295"/>
         <title>Mustafa Portfolio </title>
       </head>
+     
       <body className={roboto_mono.className}>{children}</body>
     </html>
   )
